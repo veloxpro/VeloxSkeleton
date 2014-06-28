6a7b19c6cc5809cb293a2abe1de4d625efd0f24f
veloxpro <veloxpro@gmail.com>
2014-06-28 14:19:46 -0700

    ui

--- app/layout/base.php
+++ app/layout/base.php
@@ -1,8 +1,16 @@
+<!DOCTYPE html>
 <html>
 <head>
-    <title>Some title</title>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Velox Skeleton</title>
+    <link rel="shortcut icon" type="image/png" href="favicon.png">
+    <link rel="stylesheet" href="/dist/css/styles.min.css">
+    <script>var VeloxPage = {};</script>
 </head>
 <body>
     <?php $this->renderBlock('body') ?>
+    <script src="/dist/js/main.js"></script>
 </body>
 </html>
