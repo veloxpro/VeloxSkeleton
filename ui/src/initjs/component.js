f184dfbceee5e821c6d47249936fe009d4f2319b
veloxpro <veloxpro@gmail.com>
2014-10-25 05:21:14 -0700

    sys

--- ui/src/initjs/component.js
+++
@@ -1,8 +0,0 @@
-var Velox = {};
-Velox.View = {};
-
-$(function() {
-    for (var i= 0, len = VeloxView.length; i<len; i++) {
-        Velox.View[VeloxView[i]]();
-    }
-})
--- ui/src/js/Component/Test/Test/hello.js
+++
@@ -1,3 +0,0 @@
-Velox.View['Test/Test/hello'] = function () {
-    console.log('VeloxStack Skeleton App. Test view javascript executed.');
-}
